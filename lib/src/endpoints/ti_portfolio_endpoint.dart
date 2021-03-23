9b7621d73e1d2a52360ea7807e5dc71b7c68a815
greymag <greymag@gmail.com>
2021-03-23 18:50:06 +0300

    Portfolio endpoint: method to get portfolio.

---
+++ lib/src/endpoints/ti_portfolio_endpoint.dart
@@ -0,0 +1,18 @@
+import 'package:async/async.dart';
+import 'package:dio/dio.dart';
+import 'package:tinkoff_invest/src/endpoints/ti_endpoint.dart';
+import 'package:tinkoff_invest/src/models/response/portfolio_response.dart';
+
+/// Endpoint /portfolio
+class TIPortfolioEndpoint extends TIEndpoint {
+  TIPortfolioEndpoint(Dio dio) : super(dio, 'portfolio');
+
+  /// Получение портфеля клиента
+  ///
+  /// [brokerAccountId] Номер счета (по умолчанию - Тинькофф)
+  Future<Result<PortfolioResponse>> load([String? brokerAccountId]) async {
+    final params =
+        brokerAccountId != null ? {'brokerAccountId': brokerAccountId} : null;
+    return get((d) => PortfolioResponse.fromJson(d), params);
+  }
+}
