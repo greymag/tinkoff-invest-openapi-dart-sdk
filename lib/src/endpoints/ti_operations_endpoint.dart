c1b927f43500dec7211ddbd999e33a9d22e21e6d
greymag <greymag@gmail.com>
2021-03-29 18:24:56 +0300

    Small refactoring.

--- lib/src/endpoints/ti_operations_endpoint.dart
+++ lib/src/endpoints/ti_operations_endpoint.dart
@@ -9,14 +9,13 @@ class TIOperationsEndpoint extends TIEndpoint {
 
   /// Получение списка операций
   ///
-  /// [from] Начало временного промежутка (2019-08-19T18:38:33.131642+03:00).
-  /// [to] Конец временного промежутка (2019-08-19T18:38:33.131642+03:00).
+  /// [from] Начало временного промежутка.
+  /// [to] Конец временного промежутка.
   ///
   /// [figi] Figi инструмента для фильтрации.
   /// [brokerAccountId] Номер счета (по умолчанию - Тинькофф).
   Future<Result<OperationsResponse>> load(DateTime from, DateTime to,
       {String? figi, String? brokerAccountId}) async {
-    // XXX: from/to переделать на DateTime
     final params = {'from': dateParam(from), 'to': dateParam(to)};
     if (figi != null) params['figi'] = figi;
     if (brokerAccountId != null) params['brokerAccountId'] = brokerAccountId;
