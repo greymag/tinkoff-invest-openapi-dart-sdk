05b70576c57b7da22e16ae8c3db0feb73510cf8f
greymag <greymag@gmail.com>
2021-05-18 18:54:16 +0300

    Export error response type.

--- lib/tinkoff_invest.dart
+++ lib/tinkoff_invest.dart
@@ -3,3 +3,4 @@ library tinkoff_invest;
 export 'src/tinkoff_invest_api.dart';
 export 'src/models/data/data.dart';
 export 'src/models/streaming/streaming.dart';
+export 'src/models/response/error_response.dart';
